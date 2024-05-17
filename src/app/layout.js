c8e5d8219eee79fac38c3aaf7002e2feae002b9b
litun098 <nayaklitun9@gmail.com>
2024-05-17 08:56:45 +0530

    improved seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,11 +9,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Dasmat Marndi Portfolio",
   description: "Personal portfolio page",
+  keywords: "Dasmat Marndi, portfolio, web developer, projects, contact",
+  author: "Dasmat Marndi",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="author" content={metadata.author} />
+        <title>{metadata.title}</title>
+      </head>
       <body className={inter.className}>
         <TransitionProvider>{children}</TransitionProvider>
       </body>
