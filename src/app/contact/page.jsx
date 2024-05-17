73dc2276131b414848afea9c63b626fcdf459124
litun098 <nayaklitun9@gmail.com>
2024-05-17 10:51:45 +0530

    final commit

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -35,11 +35,6 @@ const ContactPage = () => {
       );
   };
 
-  console.log(
-    process.env.NEXT_PUBLIC_PUBLIC_KEY,
-    process.env.NEXT_PUBLIC_SERVICE_ID,
-    process.env.NEXT_PUBLIC_TEMPLATE_ID
-  );
   return (
     <motion.div
       className="h-full"
